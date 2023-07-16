b6216239345d771c6d1a2f575674b1ac643514a1
mudkannaa <ashwinimudknna@gmail.com>
2023-07-16 18:35:19 +0530

    server1.4

--- OneDrive/Desktop/be/index.js
+++ OneDrive/Desktop/be/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 // data
 let notes = [
@@ -21,15 +22,56 @@ let notes = [
 ];
 
 // set the endpoints
+
+// root end point: prints hello world as an HTML
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>');
 });
 
+// fetches all resources in the collection
 app.get('/api/notes', (request, response) => {
     response.json(notes);
 });
 
+// creates a new resource based on the request data
+app.post('/api/notes', (request, response) => {
+    notes = notes.concat(request.body);
+    // console.log(request.body);
+    response.status(201).json({ message: 'note created successfully' });
+});
 
+// fetching a sigle resource
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const note = notes.find(note => note.id === id);
+    if (note) {
+        response.json(note);
+    } else {
+        response.status(404).end('id does not exist');
+    }
+});
+
+// deleting a resource
+app.delete('/api/notes/:id', (request, response) => {
+    // get the id of the resource from params
+    const id = Number(request.params.id);
+    notes = notes.filter(note => note.id !== id);
+
+    response.status(204).end();
+});
+
+// replace the entire identified resource with the request data
+app.put('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id);
+    // const noteToPut = notes.find(note => note.id === id);
+
+    const noteToPut = request.body;
+
+    notes = notes.map(note => note.id === id ? noteToPut : note);
+    response.status(200).end();
+});
+
+// Listen to the PORT for requests
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
