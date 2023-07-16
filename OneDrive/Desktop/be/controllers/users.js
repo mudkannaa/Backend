b4f632bdbbed360219091ef1b8d139d5ef2f79a5
mudkannaa <ashwinimudknna@gmail.com>
2023-07-16 20:28:32 +0530

    server2.3

---
+++ OneDrive/Desktop/be/controllers/users.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user');
+
+usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body;
+
+    // create a hashed password using the password entered by the user
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    // create a user object to store it in the database
+    const user = new User({
+        username,
+        name,
+        passwordHash
+    });
+
+    const savedUser = await user.save();
+    response.status(201).json(savedUser);
+});
+
+// route for getting all users  
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({}).populate('notes', { content: 1, important: 1});
+    response.json(users);
+});
+
+
+module.exports = usersRouter;
--- OneDrive/Desktop/be/index.js
+++ OneDrive/Desktop/be/index.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware');
 const notesRouter = require('./controllers/notes');
+const usersRouter = require('./controllers/users');
 
 // create an express app
 const app = express();
@@ -30,6 +31,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
