caad0a64673d1b5af97880d40d36f0c75b672dae
mudkannaa <ashwinimudknna@gmail.com>
2023-07-16 20:38:53 +0530

    server2.4

---
+++ OneDrive/Desktop/be/controllers/login.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const loginRouter = require('express').Router();
+const User = require('../models/user');
+const { response } = require('express');
+
+loginRouter.post('/', async (request, response) => {
+    const { username, password } = request.body;
+
+    // find the user in the database
+    const user = await User.findOne({ username });
+
+    // check the password is correct or not
+    const passwordCorrect = user === null 
+        ? false
+        : await bcrypt.compare(password, user.passwordHash);
+
+    // validate the login
+    if(!(user && passwordCorrect)){
+        return response.status(401).json({ error: 'invalid username or password' });
+    }
+
+    // generate a token for the user
+    const userForToken = {
+        username: user.username,
+        id: user._id
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET);
+
+    response
+        .status(200)
+        .send({ token, username: user.username, name: user.name });
+});
+
+module.exports = loginRouter;
--- OneDrive/Desktop/be/index.js
+++ OneDrive/Desktop/be/index.js
@@ -6,6 +6,7 @@ const config = require('./utils/config');
 const middleware = require('./utils/middleware');
 const notesRouter = require('./controllers/notes');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 // create an express app
 const app = express();
@@ -32,6 +33,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
