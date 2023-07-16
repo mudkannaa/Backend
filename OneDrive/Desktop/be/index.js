105ff232d50088ee97eb6b5db302e61b362d90a2
mudkannaa <ashwinimudknna@gmail.com>
2023-07-16 20:09:21 +0530

    server1.8

--- OneDrive/Desktop/be/index.js
+++ OneDrive/Desktop/be/index.js
@@ -2,26 +2,37 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+// middleware
 app.use(cors());
 app.use(express.json());
+
+// connect to the database
+const url = process.env.MONGODB_URI;
+
+// set the strictQuery to false, so that it will disable the strict mode for the query filters
+// mongoose will not throw any error when we use an undefined field in the query (ignored)
+mongoose.set('strictQuery', false);
+
+// to connect to the database
+mongoose.connect(url)
+    .then(result => {
+        console.log('Connected to MongoDB Database');
+    })
+    .catch((error) => {
+        console.log('Error connecting to MongoDB:', error.message);
+    })
 
 // create a model
 const Note = require('./models/note');
 
 // set the endpoints
 
-// root end point: prints hello world as an HTML
-// app.get('/', (request, response) => {
-//     response.send('<h1>Hello World!</h1>');
-// });
+const getAllNotes = require('./routes/getAllNotes');
 
 // fetches all resources in the collection
-app.get('/api/notes', (request, response) => {
-    Note.find({}, {})
-    .then((notes) => {
-        response.json(notes);
-    });
-});
+app.use('/api/notes', getAllNotes);
 
 // creates a new resource based on the request data
 app.post('/api/notes', (request, response) => {
@@ -65,8 +76,6 @@ app.delete('/api/notes/:id', (request, response) => {
         .catch((error) => {
             response.status(500).json({ error: 'Internal server error' });
         });
-
-    // response.status(204).end();
 });
 
 // replace the entire identified resource with the request data
@@ -75,6 +84,22 @@ app.put('/api/notes/:id', (request, response) => {
     const noteToPut = request.body;
 
     Note.findByIdAndUpdate(id, noteToPut)
+        .then((updatedNote) => {
+            if (!updatedNote) {
+                return response.status(404).json({ error: 'Note not found' });
+            }
+            response.json(updatedNote);
+        })
+        .catch((error) => {
+            response.status(500).json({ error: 'Internal server error' });
+        });
+});
+
+app.patch('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    const noteToPatch = request.body;
+
+    Note.findByIdAndUpdate(id, noteToPatch)
         .then((updatedNote) => {
             if (!updatedNote) {
                 return response.status(404).json({ error: 'Note not found' });
--- OneDrive/Desktop/be/mongo.js
+++ OneDrive/Desktop/be/mongo.js
@@ -10,7 +10,7 @@ const password = process.argv[2];
 
 const url =
 
-    `mongodb+srv://ashwinimudknna:tAgQYCGXoj8IqJkV@cluster0.47boroe.mongodb.net/`;
+`mongodb+srv://ashwinimudknna:${password}@cluster0.47boroe.mongodb.net/`;
 
 // set the strictQuery to false, so that it will disable the strict mode for the query filters
 // mongoose will not throw any error when we use an undefined field in the query (ignored)
