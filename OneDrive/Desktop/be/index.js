f3176ed7629e7b1d823d24351d2f1f584487a655
mudkannaa <ashwinimudknna@gmail.com>
2023-07-16 18:31:59 +0530

    server1.3

--- OneDrive/Desktop/be/index.js
+++ OneDrive/Desktop/be/index.js
@@ -1,6 +1,7 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-// data in JSON format
+// data
 let notes = [
     {
         id: 1,
@@ -19,11 +20,17 @@ let notes = [
     }
 ];
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(notes));
+// set the endpoints
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>');
 });
 
+app.get('/api/notes', (request, response) => {
+    response.json(notes);
+});
+
+
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
