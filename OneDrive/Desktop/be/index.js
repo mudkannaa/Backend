350b7efcac0c04044f1c1f0e374d20a288b7a12c
mudkannaa <ashwinimudknna@gmail.com>
2023-07-16 20:13:21 +0530

    server1.9

--- OneDrive/Desktop/be/index.js
+++ OneDrive/Desktop/be/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+// create an express app
+const app = express();
 
 // middleware
 app.use(cors());
@@ -29,87 +31,31 @@ const Note = require('./models/note');
 
 // set the endpoints
 
+// import the controllers/routes
 const getAllNotes = require('./routes/getAllNotes');
+const createNote = require('./routes/createNote');
+const getNote = require('./routes/getNote');
+const deleteNote = require('./routes/deleteNote');
+const putNote = require('./routes/putNote');
+const patchNote = require('./routes/patchNote');
 
 // fetches all resources in the collection
 app.use('/api/notes', getAllNotes);
 
 // creates a new resource based on the request data
-app.post('/api/notes', (request, response) => {
-    // prepare an object to store it in the database
-    const note = new Note(request.body);
-
-    // storing the new object to the database
-    note.save()
-        .then(result => {
-            response.status(201).json({ message: 'Note created successfully' });
-        });
-});
+app.use('/api/notes', createNote);
 
 // fetching a sigle resource
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    Note.findById(id)
-        .then((note) => {
-            if (!note) {
-                return response.status(404).json({ error: 'Note not found' });
-            }
-            response.json(note);
-        })
-        .catch((error) => {
-            response.status(500).json({ error: 'Internal server error' });
-        });
-});
+app.use('/api/notes', getNote);
 
 // deleting a resource
-app.delete('/api/notes/:id', (request, response) => {
-    // get the id of the resource from params
-    const id = request.params.id;
-
-    Note.findByIdAndDelete(id)
-        .then((deletedNote) => {
-            if (!deletedNote) {
-                return response.status(404).json({ error: 'Note not found' });
-            }
-            response.status(204).json({ message: 'Note deleted successfully' });
-        })
-        .catch((error) => {
-            response.status(500).json({ error: 'Internal server error' });
-        });
-});
+app.use('/api/notes', deleteNote);
 
 // replace the entire identified resource with the request data
-app.put('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    const noteToPut = request.body;
+app.use('/api/notes', putNote);
 
-    Note.findByIdAndUpdate(id, noteToPut)
-        .then((updatedNote) => {
-            if (!updatedNote) {
-                return response.status(404).json({ error: 'Note not found' });
-            }
-            response.json(updatedNote);
-        })
-        .catch((error) => {
-            response.status(500).json({ error: 'Internal server error' });
-        });
-});
-
-app.patch('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    const noteToPatch = request.body;
-
-    Note.findByIdAndUpdate(id, noteToPatch)
-        .then((updatedNote) => {
-            if (!updatedNote) {
-                return response.status(404).json({ error: 'Note not found' });
-            }
-            response.json(updatedNote);
-        })
-        .catch((error) => {
-            response.status(500).json({ error: 'Internal server error' });
-        });
-});
+// patch request to update the identified resource with the request data
+app.use('/api/notes', patchNote);
 
 // Listen to the PORT for requests
 const PORT = process.env.PORT || 3001;
